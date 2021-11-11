a919329680cb811d79dc839055d20bc397dab465
apmariano <sapphire.blue522@gmail.com>
2021-11-11 08:48:05 +0000

    Change to users API + addition of works API

--- app.js
+++ app.js
@@ -6,19 +6,30 @@ const port = process.env.PORT || 5000
 
 var allowedOrigins = ['http://localhost:5000', 'https://fiddle.jshell.net', 'https://skeb-explorer.herokuapp.com']
 var corsOptionsDelegate = {
-    origin: function(origin, callback){
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
+  origin: function(origin, callback){
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
     }
+  }
 }
 
 const userProfile = "https://skeb.jp/api/users/"
 const newWorks = "https://skeb.jp/api/works?sort=date&genre=art&limit=50&age=0&offset="
 
-app.get('/api/user/:id', cors(corsOptionsDelegate), (req, res) => {
+app.get('/api/users/:id/works/:works_id', cors(corsOptionsDelegate), (req, res) => {
+  const profileWorkLink = userProfile + req.params.id + "/works/" + req.params.works_id
+  axios.get(profileWorkLink, {
+    headers: {
+    Authorization: "Bearer null"
+    }
+  }).then( resp => {
+    res.send(resp.data)
+  })
+})
+
+app.get('/api/users/:id', cors(corsOptionsDelegate), (req, res) => {
   const profileLink = userProfile + req.params.id
   axios.get(profileLink, {
     headers: {
