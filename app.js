8f7dd7f0cbf63e23723a713387b0507eacb994f7
apmariano <sapphire.blue522@gmail.com>
2021-11-11 06:13:43 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const axios = require('axios')
+var cors = require('cors')
+const app = express()
+const port = 3500
+
+var allowedOrigins = ['http://localhost:3000', 'https://jsfiddle.com', 'https://skeb-explorer.herokuapp.com']
+var corsOptionsDelegate = {
+    origin: function(origin, callback){
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+const userProfile = "https://skeb.jp/api/users/"
+const newWorks = "https://skeb.jp/api/works?sort=date&genre=art&limit=50&age=0&offset="
+
+app.get('/api/user/:id', cors(corsOptionsDelegate), (req, res) => {
+  const profileLink = userProfile + req.params.id
+  axios.get(profileLink, {
+    headers: {
+      Authorization: "Bearer null"
+    }
+  }).then( resp => {
+    res.send(resp.data)
+  })
+})
+
+app.get('/api/new/art/:page', cors(corsOptionsDelegate), (req, res) => {
+  const worksPage = newWorks + (req.params.page * 50)
+  console.log("Offset:", req.params.page * 50)
+  axios.get(worksPage).then( resp => {
+    res.send(resp.data)
+  })
+})
+
+app.get("/", (req, res) => {
+  res.send("Skeb Explorer")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
