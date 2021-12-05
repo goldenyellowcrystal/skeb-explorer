8be3ab154a9e8bb1d004e6e840eb0f190b9072d9
apmariano <sapphire.blue522@gmail.com>
2021-12-05 02:49:39 +0000

    Improved Commissioner info Header

--- frontend/src/components/commissioner/UserSummary.vue
+++ frontend/src/components/commissioner/UserSummary.vue
@@ -1,9 +1,60 @@
 <template>
   <div class="user-summary">
-    User Icon: <img :src= "userInfo.userIcon" class="icon"/><br/>
-    Name: {{ userInfo.userName }} <br />
-    Screen Name: {{ userInfo.screenName }} <br />
-    Header URL: {{ userInfo.userHeader }} <br />
+    <v-img
+      max-height="175px"
+      :src="userInfo.userHeader">
+      <v-overlay
+        :absolute="true"
+        :opacity="0.3"
+      >
+      </v-overlay>
+    </v-img>
+    <v-container class="offset-65">
+      <v-row>
+        <v-col>
+          <v-row class="pa-0 flex-nowrap">
+            <v-col class="pt-0 pl-6 avatar-col" :cols="4">
+              <v-avatar size="124">
+                <img
+                  :src="userInfo.userIcon"
+                  :alt="userInfo.userName + ' icon'" />
+              </v-avatar>
+            </v-col>
+            <v-col class="pt-0" :cols="8">
+              <v-row class="d-block pb-5">
+                <div class="text-h4 text-no-wrap text-truncate header-text">
+                  {{ userInfo.userName }}
+                </div>
+                <div class="d-block text-no-wrap text-truncate header-text">
+                  @{{ userInfo.screenName }}
+
+                  <v-btn
+                    small
+                    color="primary"
+                    elevation="2"
+                    class="ms-2"
+                  >
+                    <v-icon left>
+                      mdi-eye-plus
+                    </v-icon>
+                    Watch
+                  </v-btn>                  
+                </div>
+              </v-row>
+              <v-row>
+                <div>
+                  <v-chip small outlined
+                    class="mr-1"
+                    color="orange">
+                      Commissioner
+                  </v-chip>
+                </div>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
