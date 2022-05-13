785be2c2936da8f214500f996282b03edcc2611b
Yellow <sapphire.blue522@gmail.com>
2022-05-13 20:24:56 +0800

    Making Firebase credentials work on Heroku (2)

--- utils/firebase/firebase-utils.js
+++ utils/firebase/firebase-utils.js
@@ -1,10 +1,11 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
+var serviceAcct;
 if (process.env.NODE_ENV == 'production') {
-  const serviceAcct = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+  serviceAcct = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 } else {
-  const serviceAcct = require('../../.credentials/firebase-creds.json');
+  serviceAcct = require('../../.credentials/firebase-creds.json');
 }
 
 initializeApp({
